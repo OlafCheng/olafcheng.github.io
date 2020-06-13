a26f54af6235bbac081dc5497e5648b5c54d5752
olafcheng <olafcheng@gmail.com>
2020-06-13 13:54:35 +0800

    feat: 家里人需要计算配方比例的小工具 ... 找不到 hexo 工程的源文件了, 先手动更新一下

---
+++ 2020/06/13/ration-calculator/precache-manifest.18a6c211e17681b331ff239af29a9e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fe3763d7b4f214176a9f230125c300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16359e52f4235dc91b5",
+    "url": "/static/css/main.3a11917c.chunk.css"
+  },
+  {
+    "revision": "020abfee94af9e84f9ea",
+    "url": "/static/js/2.6c8debcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c8debcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16359e52f4235dc91b5",
+    "url": "/static/js/main.69a39c64.chunk.js"
+  },
+  {
+    "revision": "8f9a2325f75f49802a11",
+    "url": "/static/js/runtime-main.3646d89b.js"
+  }
+]);
